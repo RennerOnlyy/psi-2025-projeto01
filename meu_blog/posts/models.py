b2f61d060a4fd36b489e5237719f62a643791998
RennerOnlyy <eliasrenner75@gmail.com>
2025-10-23 15:23:14 -0300

    models do projeto

--- meu_blog/posts/models.py
+++ meu_blog/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# posts/models.py
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Conteúdo")
+    publication_date = models.DateTimeField(default=timezone.now, verbose_name="Data de Publicação")
+    image = models.ImageField(upload_to='post_images/', verbose_name="Imagem")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Postagem"
+        verbose_name_plural = "Postagens"
